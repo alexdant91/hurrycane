3e047512a8e74d5fb2c6e9b9ed73d50250267d9b
Alessandro D'Antoni <alexdant91@gmail.com>
2019-02-27 17:15:57 +0100

    Add cluster support in env configuration

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ class Config {
         this.NODE_ENV = process.env.NODE_ENV || 'staging';
         this.port = 80;
         this.host = 'http://localhost';
-        this.cluster = true;
+        this.cluster = process.env.CLUSTER_ENV || false;
         this.sessionSecretKey = 'sessionsecretkey';
         this.api = {
             requestTimeLimitRange: 60 * 60 * 1000,
